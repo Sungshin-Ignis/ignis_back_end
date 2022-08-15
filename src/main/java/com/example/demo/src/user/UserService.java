9bda7e956e6bb97d1324f09d6d84c57de535578b
theunghee02 <20211150@sungshin.ac.kr>
2022-08-16 02:17:44 +0900

    [fix] 회원가입 api 수정

--- src/main/java/com/example/demo/src/user/UserService.java
+++ src/main/java/com/example/demo/src/user/UserService.java
@@ -48,7 +48,8 @@ public class UserService {
         }
         try{
             int userIdx = userDao.createUser(postUserReq);
-            userDao.insertUser(userIdx);
+            userDao.insertScore(userIdx);
+            userDao.insertIncidentNote(userIdx);
             //jwt 발급.
             // TODO: jwt는 다음주차에서 배울 내용입니다!
             String jwt = jwtService.createJwt(userIdx);
